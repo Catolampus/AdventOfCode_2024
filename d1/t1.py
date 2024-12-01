b388b19920511881959c5b5d02c0cce94dd00a3e
cucumis <tuchkov1993@gmail.com>
2024-12-01 12:25:15 +0300

    add d1

--- d1/t1.py
+++ d1/t1.py
@@ -1,1 +1,23 @@
-print("Hello, world!")
+def main() -> None:
+    l1 = []
+    l2 = []
+    with open("t1_tsk.txt", "r") as f:
+        for line in f:
+            v1, v2 = [int(v) for v in line.strip().split("  ")]
+            l1.append(v1)
+            l2.append(v2)
+    print(t1(l1, l2))
+    return
+
+
+def t1(l1: list[int], l2: list[int]) -> int:
+    l1.sort()
+    l2.sort()
+    res = 0
+    for v1, v2 in zip(l1, l2):
+        res += abs(v1 - v2)
+    return res
+
+
+if __name__ == "__main__":
+    main()
---
+++ d1/t2.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+
+def main() -> None:
+    l1 = defaultdict(int)
+    l2 = defaultdict(int)
+    with open("t1_tsk.txt", "r") as f:
+        for line in f:
+            v1, v2 = [int(v) for v in line.strip().split("  ")]
+            l1[v1] += 1
+            l2[v2] += 1
+    print(t1(l1, l2))
+    return
+
+
+def t1(l1: dict[int, int], l2: dict[int, int]) -> int:
+    res = 0
+    for key, val in l1.items():
+        res += key * l2[key]
+    return res
+
+
+if __name__ == "__main__":
+    main()
